06afc51b52affbd31991284c2e3413ae9c46753e
Jesús Silva-Rodríguez <jsilva-ibis@us.es>
2023-08-29 14:08:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,10 +28,13 @@ curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.s
 nala update -y
 
 # Installing Essentials 
-nala install  unzip wget install apt-transport-https curl build-essential  tilix  ubuntu-restricted-extras ttf-mscorefonts-installer guvcview snapd synapse -y
+nala install  unzip wget install apt-transport-https curl build-essential tilix  ubuntu-restricted-extras guvcview snapd synapse -y
 
 # Installing Other less important Programs
-nala install neofetch bashtop htop papirus-icon-theme fonts-noto-color-emoji discord gimp mricron gnome-tweaks git software-properties-common ubuntu-cleaner -y
+nala install neofetch bashtop htop papirus-icon-theme gnome-tweaks git software-properties-common ubuntu-cleaner -y
+
+# Installing even less important Programs
+nala install discord gimp github-desktop
 
 # Installing my work stuff
 nala install mricron git-lfs
@@ -43,31 +46,10 @@ flatpak install com.synology.SynologyDrive io.github.mimbrero.WhatsAppDesktop
 # Installing some snaps I need for convenience (Yes, snaps.... but in my experience these work better as snaps than flatpaks)
 snap install code mailspring notion-snap-reborn zotero-snap libreoffice
 
-# Switching to zsh
-nala install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-
-# Making .config and Moving config files and background to Pictures
-cd $builddir
-mkdir -p /home/$username/.config
-mkdir -p /home/$username/.fonts
-mkdir -p /home/$username/.themes
-mkdir -p /home/$username/.icons
-mkdir -p /home/$username/Pictures
-mkdir -p /home/$username/Pictures/backgrounds
-cp -R dotconfig/* /home/$username/.config/
-cp bg.jpg /home/$username/Pictures/backgrounds/
-mv user-dirs.dirs /home/$username/.config
-chown -R $username:$username /home/$username
-
-# Download Nordic Theme
-cd /usr/share/themes/
-git clone https://github.com/EliverLara/Nordic.git
-
 # Installing fonts
+mkdir -p /home/$username/.fonts
 cd $builddir 
-nala install fonts-font-awesome -y
+nala install fonts-font-awesome ttf-mscorefonts-installer fonts-noto-color-emoji -y
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
 unzip FiraCode.zip -d /home/$username/.fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
@@ -75,11 +57,32 @@ unzip Meslo.zip -d /home/$username/.fonts
 mv dotfonts/fontawesome/otfs/*.otf /home/$username/.fonts/
 chown $username:$username /home/$username/.fonts/*
 
-
 # Reloading Font
 fc-cache -vf
 # Removing zip Files
 rm ./FiraCode.zip ./Meslo.zip
+
+# Switching to zsh
+# Pending! Ref: https://medium.com/@satriajanaka09/setup-zsh-oh-my-zsh-powerlevel10k-on-ubuntu-20-04-c4a4052508fd
+
+
+# # Making .config and Moving config files and background to Pictures
+# cd $builddir
+# mkdir -p /home/$username/.config
+# 
+# mkdir -p /home/$username/.themes
+# mkdir -p /home/$username/.icons
+# mkdir -p /home/$username/Pictures
+# mkdir -p /home/$username/Pictures/backgrounds
+# cp -R dotconfig/* /home/$username/.config/
+# cp bg.jpg /home/$username/Pictures/backgrounds/
+# mv user-dirs.dirs /home/$username/.config
+# chown -R $username:$username /home/$username
+
+# Download Nordic Theme
+cd /usr/share/themes/
+git clone https://github.com/EliverLara/Nordic.git
+
 
 # Install Nordzy cursor
 git clone https://github.com/alvatip/Nordzy-cursors
